4d6e9a4314e81363bb6d733064c3c87aebf206bc
Bharatt1 <60268107+Bharatt1@users.noreply.github.com>
2020-04-02 02:34:00 -0400

    Add files via upload

---
+++ Assignment 7-1.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+import pandas as pd
+import os
+
+
+# In[6]:
+
+
+pwd
+
+
+# In[8]:
+
+
+variable = pd.read.csv(r"C:\\Users\\Brian\Desktop\Alabama.csv")
+
+
+# In[11]:
+
+
+variable = pd.read.csv(r"C:\Users\\Brian\Alabama.csv")
+
+
+# In[13]:
+
+
+variable = pd.read.csv(C:\Users\\Brian\Alabama.csv")
+
+
+# In[16]:
+
+
+import pandas as pd
+df = pd.read_csv(r"C:\Users\\Brian\Alabama.csv", header=None)
+df
+
