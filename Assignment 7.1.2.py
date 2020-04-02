ef4765122672785e8f0944ec6c4ffe39e105655a
Bharatt1 <60268107+Bharatt1@users.noreply.github.com>
2020-04-02 03:10:29 -0400

    Add files via upload

---
+++ Assignment 7.1.2.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[18]:
+
+
+import pandas as pd
+import os
+
+
+# In[23]:
+
+
+pwd
+
+
+# In[31]:
+
+
+import pandas as pd
+df1 = pd.read_excel(r"C:\Users\\Brian\BNK01.xls")
+df1
+
+
+# In[33]:
+
+
+df1.head
+
